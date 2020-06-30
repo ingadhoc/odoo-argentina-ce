a29cf208e6951b26113f6d80a9bbcf9116d57089
filoquin <filoquin@gmail.com>
2020-06-30 09:29:09 -0300

    [fix] Cambios para el pr

--- l10n_ar_afipws/models/res_config_settings.py
+++ l10n_ar_afipws/models/res_config_settings.py
@@ -4,6 +4,7 @@
 ##############################################################################
 from odoo import fields, models
 
+
 class ResConfigSettings(models.TransientModel):
 
     _inherit = 'res.config.settings'
@@ -11,15 +12,5 @@ class ResConfigSettings(models.TransientModel):
     afip_ws_env_type = fields.Selection(
         [('homologation', 'homologation'), ('production', 'production')],
         string='AFIP enviroment type',
+        config_parameter='afip.ws.env.type'
     )
-
-    def get_values(self):
-        res = super(ResConfigSettings, self).get_values()
-        res['afip_ws_env_type'] = self.env['ir.config_parameter'].sudo(
-        ).get_param('afip.ws.env.type', default=False)
-        return res
-
-    def set_values(self):
-        self.env['ir.config_parameter'].sudo().set_param(
-            'afip.ws.env.type', self.afip_ws_env_type)
-        super(ResConfigSettings, self).set_values()
