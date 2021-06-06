a1b72f70cf5429bb5d738c7ede64ecc619456c6a
filoquin <filoquin@gmail.com>
2021-06-06 20:39:42 -0300

    [fix] - Actualizo vistas de wizard y partner para mejorar la UX       - Agrego el campo last_update_census para guardar la fecha de actualizacion       - creo la funcion parce_census_vals para permitir extender datos del padron de otros modulos       - Elimino campos que aun no estan el la localizacion (por ejempo los del los padrones)       - ahora es la interseccion de dos arrys evitando problemas con posible campos inexistentes en el modelo partner fields_names = list(set(partner_vals) & set(fields_names))

--- l10n_ar_afipws/models/res_partner.py
+++ l10n_ar_afipws/models/res_partner.py
@@ -16,16 +16,82 @@ _logger = logging.getLogger(__name__)
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    def cuit_required(self):
-        self.ensure_one()
-        if not self.vat:
-            raise UserError(_('No CUIT configured for partner [%i] %s') % (
-                self.id, self.name))
-        return self.vat
+    last_update_census = fields.Date(
+        string='Last update census'
+    )
+
+    # Separo esto para poder heredar de otros
+    # modulos y extender los datos 
+    def parce_census_vals(self, census):
+
+        # porque imp_iva activo puede ser S o AC
+        imp_iva = census.imp_iva
+        if imp_iva == 'S':
+            imp_iva = 'AC'
+        elif imp_iva == 'N':
+            # por ej. monotributista devuelve N
+            imp_iva = 'NI'
+        
+        vals = {
+            'name': census.denominacion,
+            'street': census.direccion,
+            'city': census.localidad,
+            'zip': census.cod_postal,
+            'imp_iva_padron': imp_iva,
+            'last_update_census': fields.Date.today(),
+        }
+
+        # padron.idProvincia
+
+        ganancias_inscripto = [10, 11]
+        ganancias_exento = [12]
+        if set(ganancias_inscripto) & set(census.impuestos):
+            vals['imp_ganancias_padron'] = 'AC'
+        elif set(ganancias_exento) & set(census.impuestos):
+            vals['imp_ganancias_padron'] = 'EX'
+        elif census.monotributo == 'S':
+            vals['imp_ganancias_padron'] = 'NC'
+        else:
+            _logger.info(
+                "We couldn't get impuesto a las ganancias from padron, you"
+                "must set it manually")
+
+        if census.provincia:
+            # depending on the database, caba can have one of this codes
+            caba_codes = ['C', 'CABA', 'ABA']
+            # if not localidad then it should be CABA.
+            if not census.localidad:
+                state = self.env['res.country.state'].search([
+                    ('code', 'in', caba_codes),
+                    ('country_id.code', '=', 'AR')], limit=1)
+            # If localidad cant be caba
+            else:
+                state = self.env['res.country.state'].search([
+                    ('name', 'ilike', census.provincia),
+                    ('code', 'not in', caba_codes),
+                    ('country_id.code', '=', 'AR')], limit=1)
+            if state:
+                vals['state_id'] = state.id
+
+        if imp_iva == 'NI' and census.monotributo == 'S':
+            vals['l10n_ar_afip_responsibility_type_id'] = self.env.ref(
+                'l10n_ar.res_RM').id
+        elif imp_iva == 'AC':
+            vals['l10n_ar_afip_responsibility_type_id'] = self.env.ref(
+                'l10n_ar.res_IVARI').id
+        elif imp_iva == 'EX':
+            vals['l10n_ar_afip_responsibility_type_id'] = self.env.ref(
+                'l10n_ar.res_IVAE').id
+        else:
+            _logger.info(
+                "We couldn't infer the AFIP responsability from padron, you"
+                "must set it manually.")
+
+        return vals
 
     def get_data_from_padron_afip(self):
         self.ensure_one()
-        cuit = self.cuit_required()
+        cuit = self.ensure_vat()
 
         # GET COMPANY
         # if there is certificate for user company, use that one, if not
@@ -61,74 +127,5 @@ class ResPartner(models.Model):
         if not padron.denominacion or padron.denominacion == ', ':
             raise UserError(error_msg % (
                 self.name, cuit, 'La afip no devolvió nombre'))
-
-        # porque imp_iva activo puede ser S o AC
-        imp_iva = padron.imp_iva
-        if imp_iva == 'S':
-            imp_iva = 'AC'
-        elif imp_iva == 'N':
-            # por ej. monotributista devuelve N
-            imp_iva = 'NI'
-        
-        vals = {
-            'name': padron.denominacion,
-            'estado_padron': padron.estado,
-            'street': padron.direccion,
-            'city': padron.localidad,
-            'zip': padron.cod_postal,
-            'actividades_padron': self.actividades_padron.search(
-                [('code', 'in', padron.actividades)]).ids,
-            'impuestos_padron': self.impuestos_padron.search(
-                [('code', 'in', padron.impuestos)]).ids,
-            'imp_iva_padron': imp_iva,
-            'monotributo_padron': padron.monotributo,
-            'actividad_monotributo_padron': padron.actividad_monotributo,
-            'empleador_padron': padron.empleador == 'S' and True,
-            'integrante_soc_padron': padron.integrante_soc,
-            'last_update_padron': fields.Date.today(),
-        }
-        ganancias_inscripto = [10, 11]
-        ganancias_exento = [12]
-        if set(ganancias_inscripto) & set(padron.impuestos):
-            vals['imp_ganancias_padron'] = 'AC'
-        elif set(ganancias_exento) & set(padron.impuestos):
-            vals['imp_ganancias_padron'] = 'EX'
-        elif padron.monotributo == 'S':
-            vals['imp_ganancias_padron'] = 'NC'
-        else:
-            _logger.info(
-                "We couldn't get impuesto a las ganancias from padron, you"
-                "must set it manually")
-
-        if padron.provincia:
-            # depending on the database, caba can have one of this codes
-            caba_codes = ['C', 'CABA', 'ABA']
-            # if not localidad then it should be CABA.
-            if not padron.localidad:
-                state = self.env['res.country.state'].search([
-                    ('code', 'in', caba_codes),
-                    ('country_id.code', '=', 'AR')], limit=1)
-            # If localidad cant be caba
-            else:
-                state = self.env['res.country.state'].search([
-                    ('name', 'ilike', padron.provincia),
-                    ('code', 'not in', caba_codes),
-                    ('country_id.code', '=', 'AR')], limit=1)
-            if state:
-                vals['state_id'] = state.id
-
-        if imp_iva == 'NI' and padron.monotributo == 'S':
-            vals['l10n_ar_afip_responsibility_type_id'] = self.env.ref(
-                'l10n_ar.res_RM').id
-        elif imp_iva == 'AC':
-            vals['l10n_ar_afip_responsibility_type_id'] = self.env.ref(
-                'l10n_ar.res_IVARI').id
-        elif imp_iva == 'EX':
-            vals['l10n_ar_afip_responsibility_type_id'] = self.env.ref(
-                'l10n_ar.res_IVAE').id
-        else:
-            _logger.info(
-                "We couldn't infer the AFIP responsability from padron, you"
-                "must set it manually.")
-
+        vals = self.parce_census_vals(padron)
         return vals
