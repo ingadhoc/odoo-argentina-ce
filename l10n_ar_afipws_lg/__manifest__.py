09eaec890e7f884d9a0b7ababab90c3e5f01ba7d
Jose Romero <yjromero_arg@yahoo.com.ar>
2022-03-15 17:09:30 -0300

    Carga Inicial

---
+++ l10n_ar_afipws_lg/__manifest__.py
@@ -0,0 +1,29 @@
+{
+    "name": "Liquidación Ganadera Argentina",
+    'version': '13.0.0.0.0',
+    'category': 'Localization/Argentina',
+    'sequence': 14,
+    'author': 'ADHOC SA, Moldeo Interactive,Odoo Community Association (OCA), jjvr',
+    'license': 'AGPL-3',
+    'summary': '',
+    'depends': [
+        'l10n_ar_afipws',
+        'l10n_ar',
+        'account_debit_note',
+    ],
+    'external_dependencies': {
+    },
+    'data': [
+        'views/account_move_views.xml',
+        'views/account_journal_view.xml',
+        'views/report_invoice.xml',
+        'views/menuitem.xml',
+    ],
+    'demo': [
+    ],
+    'images': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
