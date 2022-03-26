e65a2b3d78cbb361781145a1af8b2d8e7b560162
filoquin <filoquin@gmail.com>
2022-03-26 16:27:21 -0300

    [ADD] ADD CAEA module

---
+++ l10n_ar_afipws_caea/models/account_move_ws.py
@@ -0,0 +1,42 @@
+##############################################################################
+# For copyright and license notices, see __manifest__.py file in module root
+# directory
+##############################################################################
+from odoo import fields, models, api, _
+from odoo.exceptions import UserError
+from datetime import datetime
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    def wsfe_pyafipws_caea_create_invoice(self, ws, invoice_info):
+        ws.CrearFactura(
+            invoice_info['concepto'],
+            invoice_info['tipo_doc'],
+            invoice_info['nro_doc'],
+            invoice_info['doc_afip_code'],
+            invoice_info['pos_number'],
+            invoice_info['cbt_desde'],
+            invoice_info['cbt_hasta'],
+            invoice_info['imp_total'],
+            invoice_info['imp_tot_conc'],
+            invoice_info['imp_neto'],
+            invoice_info['imp_iva'],
+            invoice_info['imp_trib'],
+            invoice_info['imp_op_ex'],
+            invoice_info['fecha_cbte'],
+            invoice_info['fecha_venc_pago'],
+            invoice_info['fecha_serv_desde'],
+            invoice_info['fecha_serv_hasta'],
+            invoice_info['moneda_id'],
+            invoice_info['moneda_ctz'],
+            invoice_info['caea'],
+            invoice_info['CbteFchHsGen'],
+        )
+
+    def wsfe_caea_request_autorization(self, ws):
+        ws.CAEARegInformativo()
