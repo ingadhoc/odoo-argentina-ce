ec87571c3307ac49768e038b0cdbd2342020d2d8
Sergio Ariel Ameghino <ariel.ameghino@gmail.com>
2024-04-12 10:03:03 -0300

    Update __manifest__.py

--- l10n_ar_afipws_fe/__manifest__.py
+++ l10n_ar_afipws_fe/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Factura Electrónica Argentina",
-    "version": "17.0",
+    "version": "17.0.1.0.0",
     "category": "Localization/Argentina",
     "sequence": 14,
     "author": "ADHOC SA, Moldeo Interactive,Odoo Community Association (OCA)",
