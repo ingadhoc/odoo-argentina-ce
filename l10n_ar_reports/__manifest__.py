0eb889672a2ed775e4fdccbc3be8f288106e74b1
Sergio Ariel Ameghino <ariel.ameghino@gmail.com>
2024-04-12 10:06:18 -0300

    Update __manifest__.py

--- l10n_ar_reports/__manifest__.py
+++ l10n_ar_reports/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Argentinian Reports (CE)",
-    "version": "17.0",
+    "version": "17.0.1.0.0",
     "category": "Localization/Argentina",
     "sequence": 14,
     "author": "ADHOC SA,Moldeo Interactive,Odoo Community Association (OCA)",
