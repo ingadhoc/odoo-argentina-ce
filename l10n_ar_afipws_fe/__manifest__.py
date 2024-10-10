32f8bb278d55cd61572d7db849f229bd632dd232
Martin Quinteros <maq@adhoc.com.ar>
2024-10-10 15:17:45 -0300

    [MIG] l10n_ar_afipws_fe: Migration to 18.0

--- l10n_ar_afipws_fe/__manifest__.py
+++ l10n_ar_afipws_fe/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Factura Electrónica Argentina",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Localization/Argentina",
     "sequence": 14,
     "author": "ADHOC SA, Moldeo Interactive,Odoo Community Association (OCA)",
@@ -23,7 +23,7 @@
     ],
     "demo": [],
     "images": [],
-    'installable': False,
+    'installable': True,
     "auto_install": False,
     "application": False,
 }
