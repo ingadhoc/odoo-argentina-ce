8604050128e1b2261e758d66f29b734d5f75fcb8
Martin Quinteros <maq@adhoc.com.ar>
2025-03-10 00:22:54 -0300

    [IMP] l10n_ar_afipws_fe: RG 5616/2024 Currency

--- l10n_ar_afipws_fe/models/account_journal.py
+++ l10n_ar_afipws_fe/models/account_journal.py
@@ -95,11 +95,3 @@ class AccountJournal(models.Model):
             },
         }
         return notification
-
-    def get_pyafipws_currency_rate(self, currency):
-        raise UserError(
-            currency.get_pyafipws_currency_rate(
-                afip_ws=self.afip_ws,
-                company=self.company_id,
-            )[1]
-        )
